461fb033478f27c15bcb376e415ef61933c3e3da
Fábio <fdsgoncalves@outlook.com>
2020-09-15 14:04:07 -0300

    BeanFactory started

---
+++ spring-demo/src/main/java/com/springdemo/springdemo/SpringHelloApp.java
@@ -0,0 +1,21 @@
+package com.springdemo.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHelloApp {
+    public static void main(String[] args) {
+        
+        // Load the spring configuration file
+        ClassPathXmlApplicationContext context = 
+            new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        // Call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // Close the context
+        context.close();
+    }
+}
