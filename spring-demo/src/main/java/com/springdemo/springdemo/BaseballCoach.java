c00f512ba59393829cc2e291b6561bed1e8e154f
Fábio <fdsgoncalves@outlook.com>
2020-09-15 15:50:36 -0300

    Dependency injection

--- spring-demo/src/main/java/com/springdemo/springdemo/BaseballCoach.java
+++ spring-demo/src/main/java/com/springdemo/springdemo/BaseballCoach.java
@@ -2,8 +2,25 @@ package com.springdemo.springdemo;
 
 public class BaseballCoach implements Coach {
     
+    // Define a private field for the dependency
+    private FortuneService fortuneService;
+
+    // Define a contructor for dependency injector
+    public BaseballCoach (FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
     @Override
     public String getDailyWorkout(){
+
         return "Spend 30 minutes on batting practice";
+    }
+
+    @Override
+    public String getDailyFortune(){
+
+        // Use my fortuneService to get a fortune
+
+        return fortuneService.getFortune();
     }
 }
--- spring-demo/src/main/java/com/springdemo/springdemo/TrackCoach.java
+++ spring-demo/src/main/java/com/springdemo/springdemo/TrackCoach.java
@@ -2,8 +2,28 @@ package com.springdemo.springdemo;
 
 public class TrackCoach implements Coach{
     
+     // Define a private field for the dependency
+     private FortuneService fortuneService;
+
+     // Temp issue fix in MyApp
+     public TrackCoach(){
+
+     }
+
+     // Define a contructor for dependency injector
+     public TrackCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout(){
         return "Run a hard 5k";
     }
+
+    @Override
+    public String getDailyFortune(){
+        return "Just Do It: " + fortuneService.getFortune();
+    }
+
+    
 }
