a8f32d1fc2b0b9b922366d6574eb8176238d3e97
Fábio <fdsgoncalves@outlook.com>
2020-09-18 20:42:24 -0300

    Beans with Java code

--- spring-demo-annotations/src/main/java/com/spring/demo/springdemoannotations/SportConfig.java
+++ spring-demo-annotations/src/main/java/com/spring/demo/springdemoannotations/SportConfig.java
@@ -1,16 +1,24 @@
 package com.spring.demo.springdemoannotations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.spring.demo")
+// @ComponentScan("com.spring.demo")
 public class SportConfig {
     
     // Define bean for our sad fortune service
-    
+    @Bean
+    public FortuneService sadFortuneService(){
+        return new SadFortuneService();
+    }
     
     // Define bean for our swim coach AND inject dependency
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 
 
     
