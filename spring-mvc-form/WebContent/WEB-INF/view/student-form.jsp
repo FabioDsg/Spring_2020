38add42966e308bfa67844fb5757c687f803487b
Fábio <fdsgoncalves@outlook.com>
2020-09-28 16:30:19 -0300

    Drop Down Lists

--- spring-mvc-form/WebContent/WEB-INF/view/student-form.jsp
+++ spring-mvc-form/WebContent/WEB-INF/view/student-form.jsp
@@ -19,6 +19,18 @@
 		Last name: <form:input path="lastName" />
 		
 		<br><br>
+		
+		Country: 
+		
+		<form:select path="country">
+		
+			<form:options items="${student.countryOptions}" />
+			
+			
+			
+		</form:select>
+		
+		<br><br>
 	
 		<input type="submit" value="Submit" />
 	
--- spring-mvc-form/src/com/spring/mvc/Student.java
+++ spring-mvc-form/src/com/spring/mvc/Student.java
@@ -6,8 +6,21 @@ public class Student {
 
 	private String firstName;
 	private String lastName;
+	
+	private String country;
+	
+	private LinkedHashMap<String, String> countryOptions;
 		
 	public Student() {
+		
+		// Populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("RU", "Russia");
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("AU", "Austrália");
 		
 	}
 
@@ -26,5 +39,19 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
 			
+	
+	
 }
