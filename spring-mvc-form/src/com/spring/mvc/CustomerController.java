c2fbcb35a6fb0151094c1c06dd26014399ea6942
Fábio <fdsgoncalves@outlook.com>
2020-10-05 23:26:45 -0300

    Debug line in theBindingResult

--- spring-mvc-form/src/com/spring/mvc/CustomerController.java
+++ spring-mvc-form/src/com/spring/mvc/CustomerController.java
@@ -39,6 +39,10 @@ public class CustomerController {
 			@Valid @ModelAttribute("customer") Customer theCustomer, 
 			BindingResult theBindingResult) {
 		
+		System.out.println("Bind result: " + theBindingResult); // Debug line
+		
+		System.out.println("\n\n\n\n");
+		
 		if (theBindingResult.hasErrors()) {
 			
 			return "customer-form";
