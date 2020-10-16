c379a8f1550ee069d7c53e5eb99cd6c248df7e7b
Fábio <fdsgoncalves@outlook.com>
2020-10-16 15:54:58 -0300

    Updating objects

---
+++ hibernate-section/src/com/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,60 @@
+package com.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.demo.entity.Student;
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// Create a session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+
+		Session session = factory.getCurrentSession();
+		
+		try {
+		
+			int studentId = 1;
+	
+			// Now get a new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			// Retrieve based on the id: primary key
+			System.out.println("\nGetting student with id: " + studentId);
+			
+			Student myStudent = session.get(Student.class, studentId);
+			
+			System.out.println("Updating student... ");
+			myStudent.setFirstName("McCartney");
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			
+			// New Code
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			// Update email for all students
+			System.out.println("Updating email for all students... ");
+			
+			session.createQuery("update Student set email='foo@mail.com'").executeUpdate();
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			
+		} finally {
+			
+			factory.close();
+		}
+	}
+
+}
