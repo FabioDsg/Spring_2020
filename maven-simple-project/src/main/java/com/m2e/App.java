895d103a2f74d1139155645dd10d7036afcf16e8
Fábio <fdsgoncalves@outlook.com>
2020-12-22 20:44:09 -0300

    Some m2e tests

---
+++ maven-simple-project/src/main/java/com/m2e/App.java
@@ -0,0 +1,13 @@
+package com.m2e;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from Maven o7!" );
+    }
+}
---
+++ mwebapp/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello from Maven webapp 07!</h2>
+</body>
+</html>
