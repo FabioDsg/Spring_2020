8a9c6f54a0404693047d454b8dee90e61356ed29
Fábio <fdsgoncalves@outlook.com>
2020-12-23 23:01:26 -0300

    Display user id and roles

---
+++ spring-security-demo-07-user-roles/src/main/java/com/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,35 @@
+package com.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages= "com.springsecurity.demo")
+public class DemoAppConfig implements WebMvcConfigurer{
+	
+	// Define a bean for viewResolver
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+    }
+
+}
---
+++ spring-security-demo-07-user-roles/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Spring Security Home Page</title>
+</head>
+<body>
+		<h2>Spring Security Company Home Page</h2>
+		
+		<p>
+			Welcome to the basic Spring Security Home Page
+		</p>
+		
+		<hr>
+			<!-- Display user name and role -->
+			<p>
+			User: <security:authentication property="principal.username"/>
+			<br><br>
+			
+			Role(s) : <security:authentication property="principal.authorities"/>
+			</p>
+		<hr>
+		
+		
+		<!-- Add a logout button -->	
+		<form:form action="${pageContext.request.contextPath}/logout" method="POST">
+		
+			<input type="submit" value="logout" />
+			
+		</form:form>
+			
+</body>
+</html>
