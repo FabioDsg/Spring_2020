e5a95f29765496916ae5f5a43e5d7f15ecfc460b
Fábio <fdsgoncalves@outlook.com>
2020-12-28 14:26:50 -0300

    Spring boot REST config, pagination and sorting

--- 24-spring-data-rest-cruddemo/src/main/java/com/springboot/cruddemo/dao/EmployeeRepository.java
+++ 24-spring-data-rest-cruddemo/src/main/java/com/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springboot.cruddemo.entity.Employee;
 
+// @RepositoryRestResource(path = "members") tell spring to use "members" instead "employees"
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// That's it ... No need to write any code LOL!
