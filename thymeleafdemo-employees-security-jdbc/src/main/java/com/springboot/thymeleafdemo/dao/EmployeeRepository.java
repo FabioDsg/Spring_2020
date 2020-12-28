fbd15e4bc762496ff844c736c9827de099314a3b
Fábio <fdsgoncalves@outlook.com>
2020-12-28 19:54:45 -0300

    Spring boot security Login/Logout with Thymeleaf

---
+++ thymeleafdemo-employees-security-jdbc/src/main/java/com/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,19 @@
+package com.springboot.thymeleafdemo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// That's it ... No need to write any code LOL!
+	
+	// Add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
+	// Search by name
+	public List<Employee> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String name, String lName);
+	
+}
